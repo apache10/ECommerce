a2407d410650af9821f813bea0d74e2c320ce741
Gaurav Gupta <gagu12ec@gmail.com>
2023-08-20 10:47:15 +1000

    Update settings.py
    removed the key

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k&j@r-j8@^$@dn_x2f@&p-5ix&vai+@57-3)*c%n$8kw-qz3wd'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
